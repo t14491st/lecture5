d1ff14baa907f8c2fddf6ee803f21ca7700b9b84
t14491st <sonosuke.takao@gmail.com>
2014-11-03 20:51:54 +0900

    課題

---
+++ main.js
@@ -0,0 +1,32 @@
+var greeting = function(message){
+  window.alert(message);
+
+
+ var output = document.querySelector("#answer");
+ output.value = message;
+};
+
+var helloWorld = function(event){
+
+ var inputA = document.querySelector("#input-a");
+ var inputB = document.querySelector("#input-b");
+
+  var a = Number(inputA.value);
+  var b = Number(inputB.value);
+
+  var answer =  a + b;
+
+  console.log("helloWorld: start");
+  greeting(answer);
+  console.log("helloWorld: end");
+};
+
+var boot = function(event){
+  console.log("boot: start");
+  var button = document.querySelector("button");
+  button.addEventListener("click", helloWorld);
+  button.addEventListener("dblclick", helloWorld);
+  console.log("boot: end");
+};
+
+window.addEventListener("load", boot);
